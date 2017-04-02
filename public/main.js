bde55073a297e701797c44c494fc049a42300dcd
NikunjVarshney <varshney.nikunj@gmail.com>
2017-04-02 15:34:41 -0400

    fixed loading sequence

--- public/main.js
+++ public/main.js
@@ -2,115 +2,123 @@ var video = document.getElementById('video'),
     hackbutton = document.getElementById('hack'),
     bgcanvas = document.getElementById('bgcamera'),
     bar = document.getElementById('bar'),
-    progress = document.getElementById('progress');
+    progress = document.getElementById('progress'),
+    ipbox = document.getElementById('glitch'),
+    userip;
 
 hackbutton.addEventListener('click', function startCamera() {
 
     var width = 1,
-        id = setInterval(frame, 25);
+        id = setInterval(frame, 45);
 
     function frame() {
         if (width >= 100) {
 
             progress.style.display = 'none';
+            hackbutton.style.display = 'none';
             clearInterval(id);
 
         } if (width === 100) {
 
-            // Pixelated Camera
+            setTimeout(function () {
 
-            window.addEventListener('resize', resizeCanvas, false);
+                ipbox.innerHTML = 'IP:' + userip;
+                ipbox.style.display = 'block';
 
-            function resizeCanvas() {
+                // Pixelated Camera
 
-                var canvas = document.getElementById('bgcanvas'),
-                    context = canvas.getContext('2d');
+                window.addEventListener('resize', resizeCanvas, false);
 
-                // canvas.width = window.innerWidth;
-                // canvas.height = window.innerHeight;
+                function resizeCanvas() {
 
-                function bgtracker() {
+                    var canvas = document.getElementById('bgcanvas'),
+                        context = canvas.getContext('2d');
 
-                    var FastTracker = function () {
-                        FastTracker.base(this, 'constructor');
-                    };
+                    // canvas.width = window.innerWidth;
+                    // canvas.height = window.innerHeight;
 
-                    tracking.inherits(FastTracker, tracking.Tracker);
+                    function bgtracker() {
 
-                    tracking.Fast.THRESHOLD = 2;
-                    FastTracker.prototype.threshold = tracking.Fast.THRESHOLD = 10;
+                        var FastTracker = function () {
+                            FastTracker.base(this, 'constructor');
+                        };
 
-                    FastTracker.prototype.track = function (pixels, width, height) {
-                        stats.begin();
-                        var gray = tracking.Image.grayscale(pixels, width, height);
-                        var corners = tracking.Fast.findCorners(gray, width, height);
-                        stats.end();
-                        this.emit('track', {
-                            data: corners
+                        tracking.inherits(FastTracker, tracking.Tracker);
+
+                        tracking.Fast.THRESHOLD = 2;
+                        FastTracker.prototype.threshold = tracking.Fast.THRESHOLD = 10;
+
+                        FastTracker.prototype.track = function (pixels, width, height) {
+                            stats.begin();
+                            var gray = tracking.Image.grayscale(pixels, width, height);
+                            var corners = tracking.Fast.findCorners(gray, width, height);
+                            stats.end();
+                            this.emit('track', {
+                                data: corners
+                            });
+                        };
+
+                        var tracker = new FastTracker();
+                        tracker.on('track', function (event) {
+                            context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+                            var corners = event.data;
+                            for (var i = 0; i < corners.length; i += 2) {
+                                context.fillStyle = '#f00';
+                                context.fillRect(corners[i], corners[i + 1], 2, 2);
+                            }
                         });
-                    };
 
-                    var tracker = new FastTracker();
-                    tracker.on('track', function (event) {
-                        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-                        var corners = event.data;
-                        for (var i = 0; i < corners.length; i += 2) {
-                            context.fillStyle = '#f00';
-                            context.fillRect(corners[i], corners[i + 1], 2, 2);
-                        }
-                    });
+                        tracking.track('#video', tracker);
+                    }
 
-                    tracking.track('#video', tracker);
+                    bgtracker();
+
                 }
 
-                bgtracker();
+                resizeCanvas();
 
-            }
+                // Main Camera
 
-            resizeCanvas();
+                function maincamera() {
 
-        } if (width === 100) {
+                    var canvas = document.getElementById('canvas'),
+                        context = canvas.getContext('2d');
 
-            // Main Camera
+                    video.width = window.innerWidth / 2;
+                    video.height = window.innerHeight / 2;
 
-            function maincamera() {
+                    canvas.width = video.width;
+                    canvas.height = video.height;
 
-                var canvas = document.getElementById('canvas'),
-                    context = canvas.getContext('2d');
+                    tracker = new tracking.ObjectTracker('face');
 
-                video.width = window.innerWidth / 2;
-                video.height = window.innerHeight / 2;
+                    tracker.setInitialScale(1);
+                    tracker.setStepSize(5);
+                    tracker.setEdgesDensity(0.1);
 
-                canvas.width = video.width;
-                canvas.height = video.height;
-
-                tracker = new tracking.ObjectTracker('face');
-
-                tracker.setInitialScale(1);
-                tracker.setStepSize(5);
-                tracker.setEdgesDensity(0.1);
-
-                tracking.track('#video', tracker, {camera: true});
-                tracker.on('track', function (event) {
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    event.data.forEach(function (rect) {
-                        context.strokeStyle = '#a64ceb';
-                        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-                        context.font = '11px Helvetica';
-                        context.fillStyle = "#fff";
-                        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-                        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                    tracking.track('#video', tracker, {camera: true});
+                    tracker.on('track', function (event) {
+                        context.clearRect(0, 0, canvas.width, canvas.height);
+                        event.data.forEach(function (rect) {
+                            context.strokeStyle = '#a64ceb';
+                            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                            context.font = '11px Helvetica';
+                            context.fillStyle = "#fff";
+                            context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                            context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                        });
                     });
-                });
-            }
+                }
 
-            maincamera();
+                maincamera();
+
+            }, 1000);
 
         } else {
 
             width++;
             bar.style.width = width + '%';
-            bar.innerHTML = width + '%';
+            hackbutton.innerHTML = 'Exposing: ' + width + '%';
 
         }
     }
